bd3d2798dd3419863f90dcfddde3973a1ae0c2ac
Anton Hromov <ahromov@oculox.com>
2023-12-14 18:01:47 +0100

    Enable the build of assembler source files

--- src/qcrashhandler.cmake
+++ src/qcrashhandler.cmake
@@ -1,6 +1,9 @@
 cmake_minimum_required(VERSION 3.10)
 
 
+# Set used languages to support assembler build as well.
+enable_language(CXX C ASM)
+
 # Initialize lists.
 set(CRASHHANDLER_HEADERS "" PARENT_SCOPE)
 set(CRASHHANDLER_SOURCES "" PARENT_SCOPE)
